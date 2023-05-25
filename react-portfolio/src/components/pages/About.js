1d43cc5804581d67dbab64f0f490f912de37798c
Antonio <robledoy@uci.edu>
2023-05-25 14:49:48 -0700

    updated About page bio

--- react-portfolio/src/components/pages/About.js
+++ react-portfolio/src/components/pages/About.js
@@ -4,16 +4,19 @@ import pfp from '../../images/Grad_Photo.png';
 const About = () => {
     return (
         <div className='bg-slate-300 p-2'>
-            <div className='font-bold flex justify text-3xl m-4 ml-10'>About Me</div>
-            <img class='flex w-40 h-40 rounded-full' src={pfp}/>
+            <div className='font-bold flex justify-center text-4xl m-4 p-2'>About Me</div>
+            <div className='flex justify-center'>
+                <img className='w-60 h-60 rounded-full' src={pfp} alt='profile picture'/>
+            </div>
 
-        <div className='inline-block text-justify mt-2 m-3 p-6 text-xl leading-loose'>
-           <p> Hello, I'm Antonio. A full-stack web developer. </p>
+    <div className='flex justify-center'>
+        <div className='mt-10 max-w-4xl text-center text-xl leading-loose'>
+           <p> Hello, my name is Antonio. I'm an avid full-stack web developer. </p>
             <br></br>
-           <p> My experience in frontend development rests on HTML, CSS, and JavaScript to create responsive and user-friendly web applications.
-            In backend development, I have worked with other languages such as Node, Express, and SQL in order to build the server-side of such 
-            applications. Some of my favorite frameworks and libraries to work with include Tailwind, React, Express, and jQuery. I am also familiar 
-            with databases and cloud hosting platforms such as Heroku, MongoDB, and MySQL. </p>
+           <p> My experience in frontend development lies in HTML, CSS, and JavaScript to create responsive and user-friendly web applications.
+            I have also taken an interest in backend development as I have worked with languages such as Node, Express, and SQL in order to build secured
+            and optimized applications. Some of my favorite frameworks and libraries to work with include Express, React, and Tailwind. I am currently familiarizing  
+            myself with databases and cloud hosting platforms such as Heroku, MongoDB, and MySQL. </p>
             <br></br>
            <p>I am always looking to expand my knowledge of both the front and back ends of development and am finding ways to implement
             new technologies into my work! I especially enjoy curating my projects through GitHub as it allows me to work in a collaborative 
@@ -21,6 +24,7 @@ const About = () => {
             <br></br>
             Let's Connect!
         </div>
+    </div>
         </div>
     )
 }
