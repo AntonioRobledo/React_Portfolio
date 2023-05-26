eb728d29400717f8e7adfbea0b66660c185b8880
Antonio <robledoy@uci.edu>
2023-05-25 21:04:50 -0700

    fixing linting errors

--- react-portfolio/src/components/Footer.js
+++ react-portfolio/src/components/Footer.js
@@ -7,13 +7,13 @@ function Footer() {
     return (
         <footer className='bg-slate-300 p-12'>
             <div className='flex justify-center gap-6 mt-28'>
-                <a href='https://github.com/AntonioRobledo' target='_blank'>
+                <a href='https://github.com/AntonioRobledo' target='_blank'  rel="noreferrer">
                     <img src={GitHub} alt='GitHub' className='rounded-full w-14 hover:opacity-60'></img>
                 </a>
-                <a href='https://www.linkedin.com/in/antonio-robledo-ysasaga-23a37a1b0/' target='_blank'>
+                <a href='https://www.linkedin.com/in/antonio-robledo-ysasaga-23a37a1b0/' target='_blank'  rel="noreferrer">
                     <img src={LinkedIn} alt='LinkedIn' className='rounded-full w-14 hover:opacity-60'></img>
                 </a>
-                <a href='https://www.instagram.com/arysasaga/' target='_blank'>
+                <a href='https://www.instagram.com/arysasaga/' target='_blank'  rel="noreferrer">
                     <img src={Instagram} alt='Instagram' className='rounded-full w-14 hover:opacity-60'></img>
                 </a>
             </div>
